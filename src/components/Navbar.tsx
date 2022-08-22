2334ea458e10300b2d6a4605eb040d867a2ef88e
Edwin Carl Flores <edwincarlflores@gmail.com>
2022-08-23 01:26:53 +0800

    Added glassmorphic effect to navbar and pagination

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,9 @@ type NavbarProps = {
 
 const Navbar: FC<NavbarProps> = ({ setResultType, setSearchQuery }) => {
   return (
-    <nav className="fixed inset-x-0 z-50 shadow-lg bg-zinc-100">
-      <div className="max-w-full px-4 mx-auto">
-        <div className="flex items-center sm:px-3 sm:space-x-4">
+    <nav className="fixed inset-x-0 z-50 bg-zinc-100 bg-opacity-70 shadow-lg backdrop-blur-lg backdrop-filter">
+      <div className="mx-auto max-w-full px-4">
+        <div className="flex items-center sm:space-x-4 sm:px-3">
           <div
             className="font-bold text-gray-700 hover:text-gray-900"
             onClick={() => {
@@ -22,12 +22,12 @@ const Navbar: FC<NavbarProps> = ({ setResultType, setSearchQuery }) => {
             }}
           >
             <a href="#" className="flex items-center">
-              <img src={GifneeyLogo} className="w-12 h-12" alt="Gifneey" />
+              <img src={GifneeyLogo} className="h-12 w-12" alt="Gifneey" />
               <span className="hidden font-mono text-lg sm:block">Gifneey</span>
             </a>
           </div>
 
-          <div className="p-4 grow">
+          <div className="grow p-4">
             <div className="w-full md:w-1/2">
               <SearchField setSearchQuery={setSearchQuery} />
             </div>
